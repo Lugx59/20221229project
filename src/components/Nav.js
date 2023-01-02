8a6794209c93917bbdb7cdab2f81bd7fc3fbb5d0
Lugx59 <panzer5959@gmail.com>
2023-01-02 15:58:11 +0900

    first page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,25 +1,55 @@
-import React from 'react';
-import './nav.css';
+import React from "react";
+import "./nav.css";
 
 const Nav = () => {
-    return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-          <div className="container-fluid">
-          <a className="navbar-brand" href="#"><img src="images/Group-166.png" alt="logo" /></a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div className="navbar-nav">
-                <a className="nav-link" href="#">홈</a>
-                <a className="nav-link" href="#">공유</a>
-                <a className="nav-link" href="#">회원가입</a>
-                <a className="nav-link" href="#">로그인</a>
-              </div>
-            </div>
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary p-0">
+      <div className="container-fluid">
+        <a className="navbar-brand p-3" href="#">
+          <img src="images/Group-166.png" alt="logo" />
+        </a>
+        <a className="nav-link" href="#">
+          홈
+        </a>
+        <a className="nav-link" href="#">
+          공유
+        </a>
+        <a className="nav-link" href="#">
+          회원가입
+        </a>
+        <a className="nav-link" href="#">
+          로그인
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-link" href="#">
+              홈
+            </a>
+            <a className="nav-link" href="#">
+              공유
+            </a>
+            <a className="nav-link" href="#">
+              회원가입
+            </a>
+            <a className="nav-link" href="#">
+              로그인
+            </a>
           </div>
-        </nav>
-    )
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
--- src/components/bang/Main.js
+++ src/components/bang/Main.js
@@ -4,32 +4,124 @@ import "./main.css";
 const Main = () => {
   return (
     <>
-      <div className="mainback">
-        <img
-          src="images/Rectangle.png"
-          class="img-fluid"
-          style={{ MaxWidth: "100%" }}
-          alt="Rectangle"
-        />
+      <div
+        className="container-xl"
+        style={{
+          width: "100%",
+          padding: "0",
+        }}
+      >
+        <div className="container-image">
+          <div className="container-text">
+            <p className="h1">
+              대출상품
+              <p className="h2" style={{ fontSize: "0.4em" }}>
+                HOME &gt; 뱅캐시 &gt; 대출상품 &gt; 일반대출
+              </p>
+            </p>
+          </div>
+          {/* <img
+            src="images/Rectangle.png"
+            className="mainImage"
+            style={{ width: "522px", margin: "0", padding: "0" }}
+            alt="rectangle"
+          /> */}
+          <div className="button-1">
+            <button className="mainButton">
+              <a href="#" target="_blank" style={{ textDecoration: "none" }}>
+                일반대출
+              </a>
+            </button>
+            <button className="mainButton-1">
+              <a
+                href="#"
+                target="_blank"
+                style={{ textDecoration: "none", color: "#3e526c" }}
+              >
+                BCAC담보대출
+              </a>
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="mainsub">
+      <div className="bg">
         <img
           src="images/Vector-image.png"
-          class="img-fluid"
-          alt="Vector-image"
+          className="img-fluid"
+          style={{ width: "100%" }}
+          alt="vector-image"
         />
-        <button type="button" class="btn btn-danger">
-          일반대출
-        </button>
-        <button type="button" class="btn btn-primary">
-          원큐가족
+        <div className="bg-white">
+          <img src="images/Vector-28.png" />
+          <input className="b-1" button="일반대출" value="일반대출" />
           <br />
-          일반대출신청
-        </button>
-        <button type="button" class="btn btn-secondary">
-          대출상담
-        </button>
+          <p className="b-2">
+            대출신청 &rarr; 심사후
+            <br />
+            대출한도 및 금리결정 (5~20%)
+          </p>
+          <br />
+          <input className="b-3" button="원큐가족" value="원큐가족" />
+          <br />
+          <p className="b-4">
+            원큐가족 누구나!
+            <br />
+            공헌 영업실적 대출
+          </p>
+          <div className="v-line"></div>
+          <br />
+          <br />
+          <br />
+          <p className="b-5">
+            실정에 따라 본사 심의 후
+            <br />
+            대출한도 및 금리 (5~10%)
+          </p>
+          <div
+            className="my-5 justify-content-around col"
+            style={{
+              height: "200px",
+              display: "block",
+              marginTop: "auto",
+              marginBottom: "auto",
+            }}
+          >
+            <button
+              type="button"
+              className="btn btn-danger col-4"
+              style={{ borderRadius: "25px", margin: "5px", fontSize: "15pt" }}
+            >
+              일반대출
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary col-4"
+              style={{
+                borderRadius: "25px",
+                fontSize: "8pt",
+                margin: "5px",
+                width: "120px",
+              }}
+            >
+              원큐가족
+              <br />
+              일반대출신청
+            </button>
+            <button
+              type="button"
+              className="btn btn-secondary col-4"
+              style={{
+                borderRadius: "25px",
+                fontSize: "15pt",
+                margin: "5px",
+              }}
+            >
+              대출상담
+            </button>
+          </div>
+        </div>
       </div>
+      <div className="container"></div>
     </>
   );
 };
